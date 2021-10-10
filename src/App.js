23cf95af81de67e685cb0b36f2f522b21bd267a5
puru <puru20010105@gmail.com>
2021-10-10 20:16:49 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Board from "./components/Board"; 
 
-const App = () =>{
+const App = () => {
   return(
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get React app.</p>
-  </>
-  ) 
+  <div>
+    <h1>TIC TAC TOE</h1>
+    <Board />
+  </div>
+  );
 }
 
 export default App;
