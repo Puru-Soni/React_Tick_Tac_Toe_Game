0ac6f2757cc53dbe4af2897943c20785ce3da01f
puru <puru20010105@gmail.com>
2021-10-12 14:02:36 +0530

    added logic, events, states, hooks

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+// Getting Hooks for functional component, while States is used for class components
 import Square from './Square'
 
 const Board = () => {
+
+    // (Hooks) useState returns a pair, the current state value and a function, that lets you change the state.
+    // useState takes one argument, which is the initial value of the state.
+    const [ board, setBoard ] = useState( Array(9).fill(null) );
+    const [ isXnext, setIsXNext ] = useState(false);
+
+    const handleSquareClick = position => {
+        if( board[position] ){
+            return;
+        }
+
+        
+        setBoard( (prev)=>{
+            return prev.map( (square, pos)=>{
+                if( pos === position){
+                    return isXnext ? "X" : "O";
+                }
+                return square;
+            })
+        })
+        setIsXNext ( prev => !prev );
+    }
+
+    const renderSquare = (position)=>{
+        return <Square value={board[position]} onClick = {()=> handleSquareClick(position)}/>
+    }
+
     return (
         <div className="board">
             {/* Tiles, div = row, button = click-able tiles */}
             
             <div className="board-row">
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+                { renderSquare(0) }
+                { renderSquare(1) }
+                { renderSquare(2) }
             </div>
-            <div  className="board-row">
-                <Square value={3} />
-                <Square value={4} />
-                <Square value={5} />
+            <div className="board-row">
+                { renderSquare(3) }
+                { renderSquare(4) }
+                { renderSquare(5) }
             </div>
-            <div  className="board-row">
-                <Square value={6} />
-                <Square value={7} />
-                <Square value={8} />
+            <div className="board-row">
+                { renderSquare(6) }
+                { renderSquare(7) }
+                { renderSquare(8) } 
             </div>
 
         </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Square = ({value}) => {
+const Square = ({value, onClick}) => {
      // either props.value or destructor and : 
     return (
-        <button type="button" className="square">{value}</button>
+
+        // event = onCLick, handler its function. 
+        <button type="button" className="square" onClick={ onClick }>
+            {value}
+        </button>
     )
    
 }
